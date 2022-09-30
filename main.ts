fb8f91d20ee18f0ad81aa6ba1c75f6711cb8606b
jacey04 <113055266+jacey04@users.noreply.github.com>
2022-09-30 14:38:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+function _7 () {
+	
+}
+function _2 () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+function _6 () {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    _9()
+    basic.pause(500)
+    _8()
+    basic.pause(500)
+    _7()
+    basic.pause(500)
+    _6()
+    basic.pause(500)
+    _5()
+    basic.pause(500)
+    _4()
+    basic.pause(500)
+    _3()
+    basic.pause(500)
+    _2()
+    basic.pause(500)
+    _1()
+    basic.pause(500)
+    _0()
+    basic.pause(500)
+})
+function _9 () {
+	
+}
+function _5 () {
+	
+}
+function _3 () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+function _8 () {
+	
+}
+function _0 () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+}
+function _4 () {
+	
+}
+function _1 () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+}
+basic.showIcon(IconNames.Tortoise)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
